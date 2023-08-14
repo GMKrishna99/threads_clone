21a3778ad968259726a18abcd560c7380a4eeb71
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-14 10:17:32 +0530

    feat : added comment functionality

--- app/(route)/thread/[id]/page.tsx
+++ app/(route)/thread/[id]/page.tsx
@@ -3,6 +3,7 @@ import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation";
+import Comment from "@/components/forms/Comment";
 
 const Page = async ({ params }: { params: { id: string } }) => {
     if (!params.id) return null;
@@ -28,6 +29,29 @@ const Page = async ({ params }: { params: { id: string } }) => {
                     createdAt={thread.createdAt}
                     comments={thread.comments}
                 />
+            </div>
+            <div className="mt-7">
+                <Comment
+                    threadId={thread._id}
+                    currentUserImg={userInfo.image}
+                    currentUserId={JSON.stringify(userInfo._id)}
+                />
+            </div>
+            <div className="mt-10">
+                {thread.children.map((childItem: any) => (
+                    <ThreadCard
+                        key={childItem._id}
+                        id={childItem._id}
+                        currentUserId={childItem?.id || ""}
+                        parentId={childItem.parentId}
+                        content={childItem.text}
+                        author={childItem.author}
+                        community={childItem.community}
+                        createdAt={childItem.createdAt}
+                        comments={childItem.comments}
+                        isComment
+                    />
+                ))}
             </div>
         </section >
     )
